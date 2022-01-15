b71f056a6deb731b42fadc85f6d1d4f043429eca
mattquinn56 <matttquinn01@gmail.com>
2022-01-15 17:02:50 -0500

    benchmarking

--- RayTrace/main.cpp
+++ RayTrace/main.cpp
@@ -4,10 +4,12 @@
 #include "environment.h"
 
 int main() {
-    // Select which scene to view. 0 is debug, 1 is run.
-    int selectscene = 1;
+    // Select which scene to view. 0 is debug, 1 is run, 2 is benchmark
+    int selectscene = 2;
     // Select whether to use clock. 0 is no, 1 is yes
     int useclk = 1;
+    // If benchmark is selected, this is how many times to run it
+    int num = 3;
 
     
     // Resolution of image, and scale. Window size is resolution * scale.
@@ -17,7 +19,7 @@ int main() {
 
     // Start clock
     clock_t t;
-    if (useclk) {
+    if (useclk && selectscene != 2) {
         t = clock();
     }
 
@@ -26,11 +28,22 @@ int main() {
     if (selectscene == 0) {
         px = testImg(xres, yres);
     }
-    else {
+    else if (selectscene == 1) {
         px = getImg(xres, yres);
     }
+    else {
+        px = testImg(xres, yres);
+        float acc = 0;
+        for (int i = 0; i < num; ++i) {
+            t = clock();
+            px = getImg(xres, yres);
+            acc += (float)(clock() - t) / CLOCKS_PER_SEC;
+            std::cout << ((float) (clock() - t)) / CLOCKS_PER_SEC << std::endl;
+        }
+        std::cout << "Average: " << acc / num << std::endl;
+    }
 
-    if (useclk) {
+    if (useclk && selectscene != 2) {
         t = clock() - t;
         std::cout << ((float)t) / CLOCKS_PER_SEC << std::endl;
     }
--- RayTrace/utils.h
+++ RayTrace/utils.h
@@ -2,7 +2,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <SFML/Graphics.hpp>
 #include <armadillo>
 
 struct Color {
